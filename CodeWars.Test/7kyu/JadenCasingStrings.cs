acfd00b367e2c9997574b791cef85e0bb5282496
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-05-27 12:50:23 +0500

    7kyu Jaden Casing Strings problem solved

---
+++ CodeWars.Test/7kyu/JadenCasingStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CodeWars.Test;
+
+public class JadenCasingStrings
+{
+    [Theory]
+    [InlineData(
+        "How can mirrors be real if our eyes aren't real",
+        "How Can Mirrors Be Real If Our Eyes Aren't Real")]
+    public void Test(string input, string expected)
+    {
+        var actual = input.ToJadenCase();
+
+        Assert.Equal(actual, expected);
+    }
+}
+
---
+++ CodeWars/7kyu/JadenCasingStrings.cs
@@ -0,0 +1,42 @@
+﻿// https://www.codewars.com/kata/5390bac347d09b7da40006f6
+
+using System;
+using System.Text;
+
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        if (phrase is null || phrase.Length == 0)
+            return phrase;
+
+        var sb = new StringBuilder(phrase.Length);
+
+        int index = 0;
+        while (index < phrase.Length)
+        {
+            var nextSpaceIndex = phrase.IndexOf(' ', index);
+            if (nextSpaceIndex == -1)
+            {
+                WordToJadenCase(sb, phrase, index, phrase.Length);
+                index = phrase.Length;
+            }
+            else if (nextSpaceIndex - index == 0)
+            {
+                sb.Append(phrase[index++]);
+            }
+            else
+            {
+                WordToJadenCase(sb, phrase, index, nextSpaceIndex);
+                index = nextSpaceIndex;
+            }
+        }
+        return sb.ToString();
+    }
+
+    private static void WordToJadenCase(StringBuilder sb, string phrase, int start, int end)
+    {
+        sb.Append(char.IsLower(phrase[start]) ? char.ToUpper(phrase[start]) : phrase[start]);
+        sb.Append(phrase, start + 1, end - start - 1);
+    }
+}
