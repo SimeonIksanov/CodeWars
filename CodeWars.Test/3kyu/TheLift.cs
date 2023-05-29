9c81eb4efff9464b6301df67446202a7c5e9a70f
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-05-30 00:51:48 +0500

    3kyu The Lift problem solved

---
+++ CodeWars.Test/3kyu/TheLift.cs
@@ -0,0 +1,172 @@
+﻿using System;
+namespace CodeWars.Test;
+
+public class DinglemouseTest
+{
+    [Fact]
+    public void TestUp()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[0], // 1
+            new int[]{5,5,5}, // 2
+            new int[0], // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 5);
+        Assert.Equal(new[] { 0, 2, 5, 0 }, result);
+    }
+
+    [Fact]
+    public void TestDown()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[0], // 1
+            new int[]{1,1}, // 2
+            new int[0], // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 5);
+        Assert.Equal(new[] { 0, 2, 1, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpAndUp()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[]{3}, // 1
+            new int[]{4}, // 2
+            new int[0], // 3
+            new int[]{5}, // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 5);
+        Assert.Equal(new[] { 0, 1, 2, 3, 4, 5, 0 }, result);
+    }
+
+    [Fact]
+    public void TestDownAndDown()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[]{0}, // 1
+            new int[0], // 2
+            new int[0], // 3
+            new int[]{2}, // 4
+            new int[]{3}, // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 5);
+        Assert.Equal(new[] { 0, 5, 4, 3, 2, 1, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpCapasity1()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[]{2,2}, // 1
+            new int[0], // 2
+            new int[0], // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 1, 2, 1, 2, 0 }, result);
+    }
+
+    [Fact]
+    public void TestDownCapasity1()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[0], // 1
+            new int[0], // 2
+            new int[0], // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[]{2,2}, // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 6, 2, 6, 2, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpAndDown1()
+    {
+        int[][] queues =
+        {
+            new int[] {6}, // G
+            new int[] {6}, // 1
+            new int[] {6}, // 2
+            new int[] {6}, // 3
+            new int[] {6}, // 4
+            new int[] {6}, // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 3, 4, 5, 6, 4, 5, 6, 5, 6, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpAndDown2()
+    {
+        int[][] queues =
+        {
+            new int[] {1}, // G
+            new int[0], // 1
+            new int[0], // 2
+            new int[] {6}, // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 1, 3, 6, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpAndDown3()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[] {2}, // 1
+            new int[] {3,3,3}, // 2
+            new int[] {1}, // 3
+            new int[0], // 4
+            new int[0], // 5
+            new int[0], // 6
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 1, 2, 3, 1, 2, 3, 2, 3, 0 }, result);
+    }
+
+    [Fact]
+    public void TestUpAndDown4()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[] {0}, // 1
+            new int[] {3,3,3}, // 2
+            new int[0], // 3
+        };
+        var result = Dinglemouse.TheLift(queues, 1);
+        Assert.Equal(new[] { 0, 2, 3, 1, 0, 2, 3, 2, 3, 0 }, result);
+    }
+}
---
+++ CodeWars/3kyu/TheLift.cs
@@ -0,0 +1,220 @@
+﻿// https://www.codewars.com/kata/58905bfa1decb981da00009e
+
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace CodeWars;
+
+public class Dinglemouse
+{
+    public static int[] TheLift(int[][] queues, int capacity)
+    {
+        List<List<Human>> peopleWaiting = ConvertQueuesToHumansList(queues);
+        var lift = new Lift((uint)capacity, peopleWaiting);
+        lift.Run();
+        return lift.FloorsStopedAt.ToArray();
+    }
+
+    private static List<List<Human>> ConvertQueuesToHumansList(int[][] queues)
+    {
+        var peopleWaiting = new List<List<Human>>();
+        for (var floor = 0; floor < queues.Length; floor++)
+        {
+            var peopleOnFloor = new List<Human>();
+            foreach (var goingto in queues[floor])
+            {
+                var human = new Human(goingto);
+                peopleOnFloor.Add(human);
+            }
+            peopleWaiting.Add(peopleOnFloor);
+        }
+
+        return peopleWaiting;
+    }
+}
+
+public class Lift
+{
+    private uint _capacity;
+    private int _currentFloor;
+    private int _minFloor;
+    private int _maxFloor;
+    private Direction _direction;
+    private readonly List<Human> _people = new();
+    private readonly List<List<Human>> _peopleWaiting;
+
+    public readonly List<int> FloorsStopedAt = new();
+
+    public Lift(uint capacity, List<List<Human>> peopleWaiting)
+    {
+        _capacity = capacity;
+        _peopleWaiting = peopleWaiting;
+        _minFloor = 0;
+        _currentFloor = _minFloor;
+        _maxFloor = peopleWaiting.Count - 1;
+        FloorsStopedAt.Add(_currentFloor);
+    }
+
+    public Direction Direction => _direction;
+    public bool IsFull => _people.Count >= _capacity;
+
+    public void Run()
+    {
+        GoTo(_currentFloor); // started here
+        while (HasWaitingPeople() || !IsEmpty())
+        {
+            PassengerOut();
+            PassengerIn();
+            int? nextStop = FindNextStopAlongDirection(_currentFloor);
+            if (!nextStop.HasValue)
+            {
+                ChangeDirection();
+                nextStop = _direction == Direction.Up
+                    ? FindLowestWaitingPassangerGoingUp()
+                    : FindHighestWaitingPassangerGoingDown();
+            }
+            if (nextStop.HasValue)
+                GoTo(nextStop.Value);
+            else
+                continue;
+        }
+        GoTo(_minFloor);
+    }
+
+
+    private void PassengerIn()
+    {
+        var newPassangers = new List<Human>();
+        var humansOnFloor = _peopleWaiting[_currentFloor];
+
+        for (int i = 0; i < humansOnFloor.Count; i++)
+        {
+            if (IsFull) break;
+            if (_direction == Direction.Up && humansOnFloor[i].To > _currentFloor)
+            {
+                PassangerEnters(newPassangers, humansOnFloor, i);
+            }
+            else if (_direction == Direction.Down && humansOnFloor[i].To < _currentFloor)
+            {
+                PassangerEnters(newPassangers, humansOnFloor, i);
+            }
+        }
+
+        foreach (var human in newPassangers)
+        {
+            humansOnFloor.Remove(human);
+        }
+
+        void PassangerEnters(List<Human> newPassangers, List<Human> humansOnFloor, int i)
+        {
+            newPassangers.Add(humansOnFloor[i]);
+            _people.Add(humansOnFloor[i]);
+        }
+    }
+
+    private void PassengerOut()
+    {
+        for (int i = _people.Count - 1; i >= 0; i--)
+        {
+            if (_people[i].To == _currentFloor)
+                _people.Remove(_people[i]);
+        }
+    }
+
+    private int? FindNextStopAlongDirection(int fromFloor)
+    {
+        int? nextPassangerStop = NextPassangerStop();
+        if (_direction == Direction.Up)
+        {
+            var lowestWaitingGoingUp = FindLowestWaitingPassangerGoingUp(fromFloor + 1);
+            return lowestWaitingGoingUp is null
+                ? nextPassangerStop
+                : nextPassangerStop is null
+                    ? lowestWaitingGoingUp.Value
+                    : Math.Min(nextPassangerStop.Value, lowestWaitingGoingUp.Value);
+        }
+        else
+        {
+            var highestWaitingGoingUp = FindHighestWaitingPassangerGoingDown(fromFloor - 1);
+            return highestWaitingGoingUp is null
+                ? nextPassangerStop
+                : nextPassangerStop is null
+                    ? highestWaitingGoingUp.Value
+                    : Math.Max(nextPassangerStop.Value, highestWaitingGoingUp.Value);
+        }
+    }
+
+    private int? NextPassangerStop()
+    {
+        if (_people.Count == 0)
+            return null;
+
+        return _direction == Direction.Up
+            ? _people.Min(h => h.To)
+            : _people.Max(h => h.To);
+    }
+
+    private int? FindLowestWaitingPassangerGoingUp()
+    {
+        return FindLowestWaitingPassangerGoingUp(_minFloor);
+    }
+    private int? FindLowestWaitingPassangerGoingUp(int fromFloor)
+    {
+        while (fromFloor <= _maxFloor)
+        {
+            if (_peopleWaiting[fromFloor].Any(human => fromFloor < human.To))
+            {
+                return fromFloor;
+            }
+            fromFloor++;
+        }
+        return null;
+    }
+
+    private int? FindHighestWaitingPassangerGoingDown()
+    {
+        return FindHighestWaitingPassangerGoingDown(_maxFloor);
+    }
+    private int? FindHighestWaitingPassangerGoingDown(int fromFloor)
+    {
+        while (_minFloor <= fromFloor)
+        {
+            if (_peopleWaiting[fromFloor].Any(human => fromFloor > human.To))
+            {
+                return fromFloor;
+            }
+            fromFloor--;
+        }
+        return null;
+    }
+
+    private void ChangeDirection()
+    {
+        _direction = _direction == Direction.Up ? Direction.Down : Direction.Up;
+    }
+
+    private void GoTo(int floor)
+    {
+        if (_currentFloor != floor)
+        {
+            FloorsStopedAt.Add(floor);
+            _currentFloor = floor;
+        }
+    }
+
+    private bool HasWaitingPeople() =>_peopleWaiting.Any(pw => pw.Count > 0);
+
+    private bool IsEmpty() => _people.Count == 0;
+}
+
+public enum Direction
+{
+    Up,
+    Down
+}
+
+public record Human(int To)
+{
+    public int To { get; private set; } = To;
+}
